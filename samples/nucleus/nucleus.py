57e1c8aa6432d1cdc711d4324b0998e83d78be20
Erik Storrs <erikstorrs@3071-al-00045.wusm-wifi.wucon.wustl.edu>
2019-06-06 15:03:28 -0500

    retraining nucleus

--- samples/nucleus/nucleus.py
+++ samples/nucleus/nucleus.py
@@ -66,33 +66,37 @@ RESULTS_DIR = os.path.join(ROOT_DIR, "results/nucleus/")
 
 # The dataset doesn't have a standard train/val split, so I picked
 # a variety of images to surve as a validation set.
+## VAL_IMAGE_IDS = [
+##     "0c2550a23b8a0f29a7575de8c61690d3c31bc897dd5ba66caec201d201a278c2",
+##     "92f31f591929a30e4309ab75185c96ff4314ce0a7ead2ed2c2171897ad1da0c7",
+##     "1e488c42eb1a54a3e8412b1f12cde530f950f238d71078f2ede6a85a02168e1f",
+##     "c901794d1a421d52e5734500c0a2a8ca84651fb93b19cec2f411855e70cae339",
+##     "8e507d58f4c27cd2a82bee79fe27b069befd62a46fdaed20970a95a2ba819c7b",
+##     "60cb718759bff13f81c4055a7679e81326f78b6a193a2d856546097c949b20ff",
+##     "da5f98f2b8a64eee735a398de48ed42cd31bf17a6063db46a9e0783ac13cd844",
+##     "9ebcfaf2322932d464f15b5662cae4d669b2d785b8299556d73fffcae8365d32",
+##     "1b44d22643830cd4f23c9deadb0bd499fb392fb2cd9526d81547d93077d983df",
+##     "97126a9791f0c1176e4563ad679a301dac27c59011f579e808bbd6e9f4cd1034",
+##     "e81c758e1ca177b0942ecad62cf8d321ffc315376135bcbed3df932a6e5b40c0",
+##     "f29fd9c52e04403cd2c7d43b6fe2479292e53b2f61969d25256d2d2aca7c6a81",
+##     "0ea221716cf13710214dcd331a61cea48308c3940df1d28cfc7fd817c83714e1",
+##     "3ab9cab6212fabd723a2c5a1949c2ded19980398b56e6080978e796f45cbbc90",
+##     "ebc18868864ad075548cc1784f4f9a237bb98335f9645ee727dac8332a3e3716",
+##     "bb61fc17daf8bdd4e16fdcf50137a8d7762bec486ede9249d92e511fcb693676",
+##     "e1bcb583985325d0ef5f3ef52957d0371c96d4af767b13e48102bca9d5351a9b",
+##     "947c0d94c8213ac7aaa41c4efc95d854246550298259cf1bb489654d0e969050",
+##     "cbca32daaae36a872a11da4eaff65d1068ff3f154eedc9d3fc0c214a4e5d32bd",
+##     "f4c4db3df4ff0de90f44b027fc2e28c16bf7e5c75ea75b0a9762bbb7ac86e7a3",
+##     "4193474b2f1c72f735b13633b219d9cabdd43c21d9c2bb4dfc4809f104ba4c06",
+##     "f73e37957c74f554be132986f38b6f1d75339f636dfe2b681a0cf3f88d2733af",
+##     "a4c44fc5f5bf213e2be6091ccaed49d8bf039d78f6fbd9c4d7b7428cfcb2eda4",
+##     "cab4875269f44a701c5e58190a1d2f6fcb577ea79d842522dcab20ccb39b7ad2",
+##     "8ecdb93582b2d5270457b36651b62776256ade3aaa2d7432ae65c14f07432d49",
+## ]
 VAL_IMAGE_IDS = [
-    "0c2550a23b8a0f29a7575de8c61690d3c31bc897dd5ba66caec201d201a278c2",
-    "92f31f591929a30e4309ab75185c96ff4314ce0a7ead2ed2c2171897ad1da0c7",
-    "1e488c42eb1a54a3e8412b1f12cde530f950f238d71078f2ede6a85a02168e1f",
-    "c901794d1a421d52e5734500c0a2a8ca84651fb93b19cec2f411855e70cae339",
-    "8e507d58f4c27cd2a82bee79fe27b069befd62a46fdaed20970a95a2ba819c7b",
-    "60cb718759bff13f81c4055a7679e81326f78b6a193a2d856546097c949b20ff",
-    "da5f98f2b8a64eee735a398de48ed42cd31bf17a6063db46a9e0783ac13cd844",
-    "9ebcfaf2322932d464f15b5662cae4d669b2d785b8299556d73fffcae8365d32",
-    "1b44d22643830cd4f23c9deadb0bd499fb392fb2cd9526d81547d93077d983df",
-    "97126a9791f0c1176e4563ad679a301dac27c59011f579e808bbd6e9f4cd1034",
-    "e81c758e1ca177b0942ecad62cf8d321ffc315376135bcbed3df932a6e5b40c0",
-    "f29fd9c52e04403cd2c7d43b6fe2479292e53b2f61969d25256d2d2aca7c6a81",
-    "0ea221716cf13710214dcd331a61cea48308c3940df1d28cfc7fd817c83714e1",
-    "3ab9cab6212fabd723a2c5a1949c2ded19980398b56e6080978e796f45cbbc90",
-    "ebc18868864ad075548cc1784f4f9a237bb98335f9645ee727dac8332a3e3716",
-    "bb61fc17daf8bdd4e16fdcf50137a8d7762bec486ede9249d92e511fcb693676",
-    "e1bcb583985325d0ef5f3ef52957d0371c96d4af767b13e48102bca9d5351a9b",
-    "947c0d94c8213ac7aaa41c4efc95d854246550298259cf1bb489654d0e969050",
-    "cbca32daaae36a872a11da4eaff65d1068ff3f154eedc9d3fc0c214a4e5d32bd",
-    "f4c4db3df4ff0de90f44b027fc2e28c16bf7e5c75ea75b0a9762bbb7ac86e7a3",
-    "4193474b2f1c72f735b13633b219d9cabdd43c21d9c2bb4dfc4809f104ba4c06",
-    "f73e37957c74f554be132986f38b6f1d75339f636dfe2b681a0cf3f88d2733af",
-    "a4c44fc5f5bf213e2be6091ccaed49d8bf039d78f6fbd9c4d7b7428cfcb2eda4",
-    "cab4875269f44a701c5e58190a1d2f6fcb577ea79d842522dcab20ccb39b7ad2",
-    "8ecdb93582b2d5270457b36651b62776256ade3aaa2d7432ae65c14f07432d49",
-]
+        '2',
+        '3'
+        ]
 
 
 ############################################################
@@ -111,7 +115,8 @@ class NucleusConfig(Config):
     NUM_CLASSES = 1 + 1  # Background + nucleus
 
     # Number of training and validation steps per epoch
-    STEPS_PER_EPOCH = (657 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
+    #STEPS_PER_EPOCH = (657 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
+    STEPS_PER_EPOCH = (20 - len(VAL_IMAGE_IDS)) // IMAGES_PER_GPU
     VALIDATION_STEPS = max(1, len(VAL_IMAGE_IDS) // IMAGES_PER_GPU)
 
     # Don't exclude based on confidence. Since we have two classes
@@ -359,16 +364,19 @@ def mask_to_rle(image_id, mask, scores):
 #  Detection
 ############################################################
 
-def detect(model, dataset_dir, subset):
+def detect(model, dataset_dir, subset, submit_dir=None):
     """Run detection on images in the given directory."""
     print("Running on {}".format(dataset_dir))
 
-    # Create directory
-    if not os.path.exists(RESULTS_DIR):
-        os.makedirs(RESULTS_DIR)
-    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
-    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
-    os.makedirs(submit_dir)
+
+    if submit_dir is None:
+        # Create directory
+        if not os.path.exists(RESULTS_DIR):
+            os.makedirs(RESULTS_DIR)
+
+        submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+        submit_dir = os.path.join(RESULTS_DIR, submit_dir)
+        os.makedirs(submit_dir)
 
     # Read dataset
     dataset = NucleusDataset()
@@ -381,6 +389,19 @@ def detect(model, dataset_dir, subset):
         image = dataset.load_image(image_id)
         # Detect objects
         r = model.detect([image], verbose=0)[0]
+        
+        converted_numpy = [r['masks'][:, :, i].tolist() for i in range(r['masks'].shape[2])]
+        results = {
+                'rois': [[int(x) for x in roi] for roi in r['rois']],
+                'masks': converted_numpy,
+                'class_ids': [int(x) for x in r['class_ids']],
+                'scores': [float(x) for x in r['scores']]
+                }
+        # save json file
+        s_id = dataset.image_info[image_id]["id"]
+        out_fp = os.path.join(submit_dir, f'{s_id}.json')
+        json.dump(results, open(out_fp, 'w'))
+
         # Encode image to RLE. Returns a string of multiple lines
         source_id = dataset.image_info[image_id]["id"]
         rle = mask_to_rle(source_id, r["masks"], r["scores"])
@@ -389,9 +410,11 @@ def detect(model, dataset_dir, subset):
         visualize.display_instances(
             image, r['rois'], r['masks'], r['class_ids'],
             dataset.class_names, r['scores'],
-            show_bbox=False, show_mask=False,
+            show_bbox=True, show_mask=False,
             title="Predictions")
         plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+
+
 
     # Save to csv file
     submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
@@ -427,6 +450,11 @@ if __name__ == '__main__':
     parser.add_argument('--subset', required=False,
                         metavar="Dataset sub-directory",
                         help="Subset of dataset to run prediction on")
+
+    parser.add_argument('--submission-dir', required=True,
+                        metavar="submission directory", default=os.getcwd(),
+                        help="directory to put submissions in")
+
     args = parser.parse_args()
 
     # Validate arguments
@@ -486,7 +514,7 @@ if __name__ == '__main__':
     if args.command == "train":
         train(model, args.dataset, args.subset)
     elif args.command == "detect":
-        detect(model, args.dataset, args.subset)
+        detect(model, args.dataset, args.subset, submit_dir=args.submission_dir)
     else:
         print("'{}' is not recognized. "
               "Use 'train' or 'detect'".format(args.command))
